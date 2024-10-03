2c3552dcdb8360c2bbf70477079aa708a73300c9
andrei <andreimandrique3@gmail.com>
2024-10-03 14:39:48 +0800

    program the 4th function

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-import calculator from "./calculator.js";
+const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+const alphabet = alphabetString.split("");
 
-console.log(calculator.div(6, 2));
+let key = 3;
+let word = "zyxwabcd";
+const wordArray = word.split("");
+const stepOneArray = [];
+const stepTwoArray = [];
+const stepThreeArray = [];
+
+for (const item of wordArray) {
+  const indexOfLetters = alphabetString.indexOf(item);
+  stepOneArray.push(indexOfLetters);
+}
+
+for (const item of stepOneArray) {
+  const addKey = item + key;
+  stepTwoArray.push(addKey);
+}
+
+for (const item of stepTwoArray) {
+  const minusKey = item - 25;
+
+  if (minusKey < 0) {
+    stepThreeArray.push(item);
+  } else {
+    stepThreeArray.push(minusKey);
+  }
+}
+
+console.log(alphabet);
+console.log(stepOneArray);
+console.log(stepTwoArray);
+console.log(stepThreeArray);
