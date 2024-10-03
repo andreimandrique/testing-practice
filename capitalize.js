0b8b92380dc3392b775704059c403bee214f739b
andrei <andreimandrique3@gmail.com>
2024-10-03 13:25:43 +0800

    add 3 function

---
+++ capitalize.js
@@ -0,0 +1,5 @@
+function capitalize(word) {
+  return word[0].toUpperCase() + word.substring(1);
+}
+
+export default capitalize;
---
+++ index.js
@@ -0,0 +1,3 @@
+import calculator from "./calculator.js";
+
+console.log(calculator.div(6, 2));
